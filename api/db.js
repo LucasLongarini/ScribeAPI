e85425f3e7758932db8d7c0bf686929b3c7a6243
Lucas Longarini <lucaslongarini0@gmail.com>
2019-04-04 22:00:42 -0600

    added database

--- api/db.js
+++ api/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 var connection = mysql.createPool({
     connectionLimit:10,
-    host: "localhost",
-    user: "root",
-    password: "samson",
-    database: "scribe_db",
+    host: "us-cdbr-iron-east-03.cleardb.net",
+    user: "bce2f9b4bfc9a2",
+    password: "84960ad7",
+    database: "heroku_f10f3db6c1f027d",
     multipleStatements: true
 })
 
